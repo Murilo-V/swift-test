decee1bc15f67917f30cd0ade9be27d73e750cdc
Murilo-V <alveiramurilo@hotmail.com>
2022-05-24 14:58:49 -0300

    Initial Commit

---
+++ swift-test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-test
+//
+//  Created by Murilo Alves on 24/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-test/swift_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_testApp.swift
+//  swift-test
+//
+//  Created by Murilo Alves on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
